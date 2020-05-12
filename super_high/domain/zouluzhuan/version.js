2679611c6d49314a0111864c41b61297f9992642
zengruiming <ruiming@hotmail.com>
2020-05-12 19:10:57 +0800

    ios更新至1.2.10

--- super_high/domain/zouluzhuan/version.js
+++ super_high/domain/zouluzhuan/version.js
@@ -1,10 +1,10 @@
 function version() {
     //ios版本
-    this.myIosVersion = '1.2.9'
+    this.myIosVersion = '1.2.10'
     //Android版本
-    this.myAndroidVersion = '1.2.9'
+    this.myAndroidVersion = '1.3.0'
     this.device = 'android'
     this.source = 'Z1001'
-    this.versionCode = '143'
+    this.versionCode = '145'
 }
 module.exports = new version()
