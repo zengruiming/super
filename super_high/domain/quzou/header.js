c38e5caa4a05e2bd0ba159f3323f46873fcad6c7
zengruiming <ruiming@hotmail.com>
2020-08-14 16:19:16 +0800

    ios更新至1.2.13

--- super_high/domain/quzou/header.js
+++ super_high/domain/quzou/header.js
@@ -3,45 +3,44 @@ var str2Obj = require('../../utils/str2Obj.js')
 function QuzouHeader() {
 //iPhone的微信
     this.wechatHeader = str2Obj(unescape('Host: mini1.91quzou.com\n' +
-        'appId: MQUZOU2020X0U1220PS34UG1AXB0I1K0Q2AU9\n' +
+        'appId: MQUZOUA2F0Z200IYF8Q1404PY08R06X6GF2E3\n' +
+        'Content-Type: application/x-www-form-urlencoded\n' +
+        'Accept: */*\n' +
+        'appName: MINI\n' +
+        'appVersion: 3.3.0\n' +
         'source: 2001\n' +
+        'accessToken: +NEflO3uj02MBMS5Y1lDrWp6h2nk+NkrJvSsGhwGPydCKHkNptJO7Hd+bfx9FzGfFGg5r8vywHuzQM+RsPV1I22f6dfczLAoBmKNqJxFCT+j2A+yyNraZZAf5wQ59tDxOZ6eS5Zp0J1rb0XCdYGm9O6bZrMbM//Q0Ij3JD+IE9uw5kqpZvwt9Q==\n' +
         'Cache-Control: no-cache\n' +
-        'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 MicroMessenger/7.0.9(0x17000929) NetType/WIFI Language/zh_CN\n' +
-        'Proxy-Connection: keep-alive\n' +
+        'Accept-Language: zh-cn\n' +
+        'Accept-Encoding: gzip, deflate, br\n' +
+        'Content-Length: 9\n' +
+        'appMode: PROD\n' +
+        'appMuId: EE0524356A3141CC9FA5F5C44632B7B7\n' +
+        'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.14(0x17000e2e) NetType/WIFI Language/zh_CN\n' +
         'appBundleID: QUZOU_MINI\n' +
         'Referer: https://servicewechat.com/wx9681faeb5e00cdf0/38/page-frame.html\n' +
-        'appMode: PROD\n' +
-        'appVersion: 3.3.0\n' +
-        'appMuId: 46C4E2C99C6A423CA1629A08C1D29556\n' +
         'MINITYPE: QUZOUMINI\n' +
-        'accessToken: +NEflO3uj02MBMS5Y1lDrWp6h2nk+NkrJvSsGhwGPydCKHkNptJO7KGAe6dsVzx+XNSA8G8U5pwcEkT/jmwz/Qc+bLg/4n4fDulDEFNWHOKgfaVhatqzOJz6qUJF1ibR3BmH/D7LWPBLCug1ZvXZmxWn6Hq6Nveoz7ASyxArSQD40vbm6fnfkg==\n' +
-        'Content-Length: 9\n' +
-        'appName: MINI\n' +
-        'Connection: keep-alive\n' +
-        'Accept-Language: zh-cn\n' +
-        'Accept: */*\n' +
-        'Content-Type: application/x-www-form-urlencoded\n' +
-        'Accept-Encoding: gzip, deflate\n'))
+        'Connection: keep-alive\n'))
 
 //iPhone的app
     this.appHeader = str2Obj(unescape('Host: mobile01.91quzou.com\n' +
-        'appId: QUZUOO2C01YTQYOH9Q100207N063H7ES2Z43\n' +
-        'phoneModel: iPhoneSE\n' +
+        'appId: QUZUOIX2020M080B300ZES4P1W3TN2Y0LS68\n' +
+        'phoneModel: iPhone XS\n' +
         'Accept: */*\n' +
-        'Proxy-Connection: keep-alive\n' +
         'appName: QZI\n' +
         'appVersion: 4.6.0\n' +
         'source: 1004\n' +
-        'accessToken: +NEflO3uj02MBMS5Y1lDrWp6h2nk+NkrJvSsGhwGPyfs4wcVLp9cgApfWcz6kQlck+vttQl74CMPo9HibM6F9tPtHGhWTxf0SgaTS0fuBntA0NzEEDZJbQWwD57Capn1p9ey43QoDRqJEhPWJSynhlBINYoWs0Sw9ABu0ZMUSJVatUNmk8Gu7Q==\n' +
+        'accessToken: +NEflO3uj02MBMS5Y1lDrWp6h2nk+NkrJvSsGhwGPydIid2ESPmmu8shrIWzN5GdglwN04Iaq8cKH6fq71BWWErevnyDT9roTGTBjZOz7hXh47UJOsD1C/LovUk/O2B2KRsPfZglW+7lfUHro48ticWQL+IGqP/KHirnGnKnKqfFygsVUsfGZg==\n' +
         'Accept-Encoding: gzip;q=1.0, compress;q=0.5\n' +
         'Accept-Language: zh-Hans-CN;q=1.0, en-CN;q=0.9, zh-Hans;q=0.8, en-US;q=0.7, de-CH;q=0.6\n' +
         'Content-Type: application/x-www-form-urlencoded; charset=utf-8\n' +
-        'appMuId: 46C4E2C99C6A423CA1629A08C1D29556\n' +
+        'Content-Length: 0\n' +
         'appMode: PROD\n' +
+        'appMuId: EE0524356A3141CC9FA5F5C44632B7B7\n' +
         'User-Agent: \n' +
         'appBundleID: QUZUO_VIP_IOS\n' +
         'Connection: keep-alive\n' +
-        'osVersion: 9.3.2\n'))
+        'osVersion: 13.5\n'))
 }
 
 // console.log(str2Obj(v1))
